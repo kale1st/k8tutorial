85a6a49e9a5559f21e7b70bec1f767c69eac83ce
kaleaziz <aziz.kale@emm-it.de>
2024-12-01 09:57:00 +0100

    secret added

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+require("dotenv").config(); // dotenv configuration
+
 app.get("/", (req, res) => {
   res.send("Hello K8s");
+});
+
+app.get("/secret", (req, res) => {
+  res.send("my secret: " + process.env.MY_SECRET);
 });
 
 app.listen(port, () => {
